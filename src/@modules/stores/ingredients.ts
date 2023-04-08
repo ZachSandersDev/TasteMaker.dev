31144eae27a5ff6d4ad8eb755b25b26742c6bd13
Zach S <zachzachzach2@gmail.com>
2023-04-08 10:29:33 -0700

    Formatting

--- src/@modules/stores/ingredients.ts
+++ src/@modules/stores/ingredients.ts
@@ -1,19 +1,20 @@
 import { atom } from "recoil";
 
 import { setRecoil } from "recoil-nexus";
+
 import { getIngredientsLive } from "../api/ingredients";
 
 export const ingredientStore = atom<{
   listener: () => void, loading: boolean, ingredients: string[]
 }>({
-  key: 'ingredientStore',
+  key: "ingredientStore",
   default: { listener: () => undefined, loading: false, ingredients: [] }
 });
 
 export function listenForIngredients() {
   const listener = getIngredientsLive((ingredients) => {
-    setRecoil(ingredientStore, (state) => ({ ...state, ingredients: Object.keys(ingredients), loading: false }))
-  })
+    setRecoil(ingredientStore, (state) => ({ ...state, ingredients: Object.keys(ingredients), loading: false }));
+  });
 
-  setRecoil(ingredientStore, (state) => ({ ...state, loading: true, listener }))
+  setRecoil(ingredientStore, (state) => ({ ...state, loading: true, listener }));
 }
