775735c756981a366360f57cdc0e13d9f85cd6c9
Zach S <zachzachzach2@gmail.com>
2023-04-12 23:26:59 -0700

    Rework selector dialog to support folders

--- src/components/Dialogs/RecipeSelectorDialog.tsx
+++ src/components/Dialogs/RecipeSelectorDialog.tsx
@@ -1,20 +1,22 @@
+import { useState } from "react";
 import { atom, useRecoilState, useRecoilValue } from "recoil";
 import { setRecoil } from "recoil-nexus";
 
 import { recipeStore } from "../../@modules/stores/recipes";
+import { treeStore } from "../../@modules/stores/tree";
 
 import { Recipe } from "../../@modules/types/recipes";
-import { RecipeItem } from "../RecipeItem";
+import RecipeTree from "../RecipeTree/RecipeTree";
 
 import "./RecipeSelectorDialog.scss";
 
 const recipeSelectorDialog = atom<{
   resolve?: (r?: Recipe) => void;
   reject?: (e: Error) => void;
-    }>({
-      key: "recipeSelectorDialog",
-      default: {},
-    });
+}>({
+  key: "recipeSelectorDialog",
+  default: {},
+});
 
 export function selectRecipe() {
   return new Promise<Recipe | undefined>((resolve, reject) => {
@@ -23,6 +25,8 @@ export function selectRecipe() {
 }
 
 export default function RecipeSelectorDialog() {
+  const [folderStack, setFolderStack] = useState<string[]>([]);
+  const { tree } = useRecoilValue(treeStore);
   const { recipes } = useRecoilValue(recipeStore);
   const [{ resolve, reject }, setDialogState] =
     useRecoilState(recipeSelectorDialog);
@@ -34,12 +38,15 @@ export default function RecipeSelectorDialog() {
     }
   };
 
-  const rej = (e: Error) => {
-    if (reject) {
-      reject(e);
-      setDialogState({});
+  const handleClick = (id: string | number, isRecipe: boolean) => {
+    if (isRecipe) {
+      return res(recipes.find((r) => r._id === String(id)));
     }
+
+    setFolderStack([...folderStack, String(id)]);
   };
+
+  const currentFolder = tree.find((f) => String(f.id) === folderStack.at(-1));
 
   if (!resolve || !reject) {
     return null;
@@ -48,14 +55,23 @@ export default function RecipeSelectorDialog() {
   return (
     <>
       <div className="ra-dialog ra-card recipe-selector-dialog">
-        <header className="ra-header">
-          <h3>Select Recipe</h3>
+        <header
+          className="ra-header"
+          style={{ justifyContent: "flex-start", gap: "var(--spacing)" }}
+        >
+          {currentFolder && (
+            <button
+              className="material-symbols-rounded icon-button"
+              onClick={() => setFolderStack((f) => f.slice(0, f.length - 1))}
+            >
+              chevron_left
+            </button>
+          )}
+          <h3>{currentFolder ? currentFolder.text : "Select Recipe"}</h3>
         </header>
 
         <div className="ra-list">
-          {recipes.map((r) => (
-            <RecipeItem key={r._id} recipeId={r._id} onClick={() => res(r)} />
-          ))}
+          <RecipeTree folderId={folderStack.at(-1)} onClick={handleClick} />
         </div>
       </div>
       <div className="ra-dialog-cover" onClick={() => res(undefined)}></div>
