0d961586f2c63a1d14fc739a14779ae39204cf41
Zach S <zachzachzach2@gmail.com>
2023-04-13 12:01:42 -0700

    Add dialog for editing ingredients on mobile

---
+++ src/components/Dialogs/EditIngredientDialog.tsx
@@ -0,0 +1,140 @@
+import { useEffect, useRef, useState } from "react";
+import { atom, useRecoilState, useRecoilValue } from "recoil";
+import { setRecoil } from "recoil-nexus";
+
+import {
+  addIngredient,
+  deleteIngredient,
+} from "../../@modules/api/ingredients";
+
+import { ingredientStore } from "../../@modules/stores/ingredients";
+import Input from "../Input";
+import SwipeToDelete from "../SwipeToDelete";
+
+import "./EditIngredientDialog.scss";
+
+const editIngredientDialog = atom<{
+  resolve?: (r?: string) => void;
+  reject?: (e: Error) => void;
+  ingredient?: string;
+}>({
+  key: "editIngredientDialog",
+  default: {},
+});
+
+export function editIngredient(ingredient: string) {
+  return new Promise<string | undefined>((resolve, reject) => {
+    setRecoil(editIngredientDialog, { resolve, reject, ingredient });
+  });
+}
+
+export default function EditIngredientDialog() {
+  const { ingredients } = useRecoilValue(ingredientStore);
+  const [{ resolve, reject, ingredient }, setDialogState] =
+    useRecoilState(editIngredientDialog);
+
+  const [localIngredient, setLocalIngredient] = useState<string>(
+    ingredient || ""
+  );
+
+  const inputRef = useRef<HTMLInputElement>();
+
+  useEffect(() => {
+    setLocalIngredient(ingredient || "");
+  }, [ingredient]);
+
+  useEffect(() => {
+    if (resolve && reject && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [resolve, reject]);
+
+  const res = (i?: string) => {
+    if (resolve) {
+      resolve(i);
+      setDialogState({});
+    }
+  };
+
+  const handleSaveIngredient = (i?: string) => {
+    addIngredient(i || localIngredient);
+    res(i || localIngredient);
+  };
+
+  const handleDeleteIngredient = (i: string) => {
+    const confirmed = window.confirm(
+      `Are you sure you want to delete ingredient "${i}"?`
+    );
+    if (confirmed) deleteIngredient(i);
+  };
+
+  if (!resolve || !reject) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="ra-dialog ra-card edit-ingredient-dialog">
+        <header className="ra-card-header">
+          <h2>Editing Ingredient</h2>
+        </header>
+
+        <Input
+          // @ts-expect-error Don't know don't give
+          ref={inputRef}
+          type="text"
+          className="ra-input"
+          value={localIngredient || ""}
+          onChange={(e) => setLocalIngredient(e.target.value.toLowerCase())}
+          placeholder="ingredient"
+          onKeyUp={(e) => {
+            if (e.key === "Enter") {
+              handleSaveIngredient();
+            }
+          }}
+          after={
+            <button
+              className="icon-button-sm material-symbols-rounded"
+              onClick={() => setLocalIngredient("")}
+            >
+              clear
+            </button>
+          }
+        />
+
+        <div className="edit-ingredient-dialog-content">
+          {ingredients
+            .sort()
+            .filter((i) =>
+              i.toLowerCase().includes(localIngredient.toLowerCase())
+            )
+            .map((i) => (
+              <SwipeToDelete key={i} onDelete={() => handleDeleteIngredient(i)}>
+                <button
+                  className="ra-option"
+                  onClick={() => {
+                    handleSaveIngredient(i);
+                  }}
+                >
+                  {i}
+                </button>
+              </SwipeToDelete>
+            ))}
+        </div>
+        <div className="ra-actions">
+          <button className="chip-button-naked" onClick={() => res(undefined)}>
+            Cancel
+          </button>
+          <button
+            className="chip-button"
+            onClick={() => handleSaveIngredient()}
+          >
+            <i className="material-symbols-rounded">save</i>
+            Save
+          </button>
+        </div>
+      </div>
+      <div className="ra-dialog-cover" onClick={() => res(undefined)}></div>
+    </>
+  );
+}
--- src/components/IngredientItem.tsx
+++ src/components/IngredientItem.tsx
@@ -4,7 +4,9 @@ import { useRecoilValue } from "recoil";
 import { addIngredient } from "../@modules/api/ingredients";
 import { ingredientStore } from "../@modules/stores/ingredients";
 import { Ingredient } from "../@modules/types/recipes";
+import useMediaQuery from "../@modules/utils/useMediaQuery";
 
+import { editIngredient } from "./Dialogs/EditIngredientDialog";
 import SwipeToDelete from "./SwipeToDelete";
 
 import "./IngredientItem.scss";
@@ -21,6 +23,7 @@ export default function IngredientItem({
   deleteIngredient,
 }: IngredientItemProps) {
   const { ingredients } = useRecoilValue(ingredientStore);
+  const isMobile = useMediaQuery("(max-width: 999px)");
 
   const controls = useDragControls();
 
@@ -41,59 +44,109 @@ export default function IngredientItem({
         className="ingredient-item-container"
         onDelete={deleteIngredient}
       >
-        <div className="ingredient-item-swipe">
-          <span>&bull;</span>
-          <input
-            className="ra-input value-field"
-            placeholder="amt."
-            type="number"
-            value={ingredient.value}
-            size={ingredient.value.length}
-            onChange={(e) => setIngredientValue("value", e.target.value)}
-          />
-          <input
-            className="ra-input unit-field"
-            placeholder="unit"
-            list="unit-options"
-            value={ingredient.units}
-            onChange={(e) => setIngredientValue("units", e.target.value)}
-          />
-          <datalist id="unit-options">
-            <option value="cups" />
-            <option value="tbsp" />
-            <option value="tsp" />
-            <option value="g" />
-            <option value="lbs" />
-            <option value="oz" />
-            <option value="fl oz" />
-          </datalist>
+        {isMobile ? (
+          <div className="ingredient-item-swipe">
+            <input
+              className="ra-input value-field"
+              placeholder="amt."
+              type="number"
+              value={ingredient.value}
+              size={ingredient.value.length}
+              onChange={(e) => setIngredientValue("value", e.target.value)}
+            />
+            <input
+              className="ra-input unit-field"
+              placeholder="unit"
+              list="unit-options"
+              value={ingredient.units}
+              onChange={(e) => setIngredientValue("units", e.target.value)}
+            />
+            <datalist id="unit-options">
+              <option value="cups" />
+              <option value="tbsp" />
+              <option value="tsp" />
+              <option value="g" />
+              <option value="lbs" />
+              <option value="oz" />
+              <option value="fl oz" />
+            </datalist>
 
-          <input
-            className="ingredient-field ra-input"
-            placeholder="ingredient"
-            list="ingredient-options"
-            value={ingredient.ingredient}
-            onChange={(v) => setIngredientValue("ingredient", v.target.value)}
-            onKeyUp={(e) => {
-              if (e.key === "Enter") {
-                addIngredient(ingredient.ingredient);
-              }
-            }}
-          />
-          <datalist id="ingredient-options">
-            {ingredients?.map((i) => (
-              <option key={i} value={i} />
-            ))}
-          </datalist>
+            <button
+              className="ingredient-field ra-input"
+              onClick={async () => {
+                const newIngredient = await editIngredient(
+                  ingredient.ingredient
+                );
+                if (newIngredient) {
+                  setIngredientValue("ingredient", newIngredient);
+                }
+              }}
+            >
+              {ingredient.ingredient}
+            </button>
 
-          <div
-            onPointerDown={(e) => controls.start(e)}
-            style={{ touchAction: "none" }}
-            className="material-symbols-rounded drag-handle"
-          >
-            drag_indicator
+            <div
+              onPointerDown={(e) => controls.start(e)}
+              style={{ touchAction: "none" }}
+              className="material-symbols-rounded drag-handle"
+            >
+              drag_indicator
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="ingredient-item-swipe">
+            <input
+              className="ra-input value-field"
+              placeholder="amt."
+              type="number"
+              value={ingredient.value}
+              size={ingredient.value.length}
+              onChange={(e) => setIngredientValue("value", e.target.value)}
+            />
+            <input
+              className="ra-input unit-field"
+              placeholder="unit"
+              list="unit-options"
+              value={ingredient.units}
+              onChange={(e) => setIngredientValue("units", e.target.value)}
+            />
+            <datalist id="unit-options">
+              <option value="cups" />
+              <option value="tbsp" />
+              <option value="tsp" />
+              <option value="g" />
+              <option value="lbs" />
+              <option value="oz" />
+              <option value="fl oz" />
+            </datalist>
+
+            <input
+              className="ingredient-field ra-input"
+              placeholder="ingredient"
+              list="ingredient-options"
+              value={ingredient.ingredient}
+              onChange={(v) => setIngredientValue("ingredient", v.target.value)}
+              onKeyUp={(e) => {
+                if (e.key === "Enter") {
+                  addIngredient(ingredient.ingredient);
+                }
+              }}
+            />
+            <datalist id="ingredient-options">
+              {ingredients?.map((i) => (
+                <option key={i} value={i} />
+              ))}
+            </datalist>
+
+            <div
+              onPointerDown={(e) => controls.start(e)}
+              style={{ touchAction: "none" }}
+              className="material-symbols-rounded drag-handle"
+            >
+              drag_indicator
+            </div>
+          </div>
+        )}
       </SwipeToDelete>
     </Reorder.Item>
   );
