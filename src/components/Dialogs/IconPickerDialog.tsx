d39813b86ddd9694ff8588f26e4f028f388ace33
Zach S <zachzachzach2@gmail.com>
2023-05-14 10:47:42 -0700

    Fix import errors

--- src/components/Dialogs/IconPickerDialog.tsx
+++ src/components/Dialogs/IconPickerDialog.tsx
@@ -2,7 +2,7 @@ import { ReactNode, createElement, useEffect, useRef, useState } from "react";
 import "emoji-picker-element";
 
 import Button from "../../@design/components/Button/Button";
-import { ImageField } from "../../@modules/types/recipes";
+import { ImageField } from "../../@modules/types/imageField";
 import classNames from "../../@modules/utils/classNames";
 
 import ImageUpload from "../ImageUpload";
